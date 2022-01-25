2b5630367ea8667444e784211a57aac59272ccec
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2022-01-25 14:32:15 +0100

    add style, content and background to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,21 @@
 import react from "react";
+import image from "../jean-philippe-delberghe-75xPHEQBmvA-unsplash.jpg";
 
 function Home() {
     return (
-        <div>
-            <h1>Home page!</h1>
-        </div>
-    )
+        <main>
+            <img
+                src={image}
+                alt="abstract image"
+                className="absolute object-cover w-full h-full"
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+                    Hejsan! I'm Amondi
+                </h1>
+            </section>
+        </main>
+    );
 }
 
 export default Home;
