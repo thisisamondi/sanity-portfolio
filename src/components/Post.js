4f2c58f4b971df852fef6b70512307898b94490c
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2022-01-26 22:11:11 +0100

    add function to singlePost.js, tailwind config file and project.js file

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,4 @@
-import { react, useState, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../client.js";
 
